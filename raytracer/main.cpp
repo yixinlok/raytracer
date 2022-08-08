0b9e79c6acf70a9de5c432747004addf73618d8d
yixinlok <yixinlok@gmail.com>
2022-08-08 11:52:57 +0700

    Initial Commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by Yixin on 08/08/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
