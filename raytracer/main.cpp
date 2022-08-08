f063cb6b47adfeae81ebc599b108e3f0183f9a2e
yixinlok <yixinlok@gmail.com>
2022-08-08 16:22:07 +0700

    basic

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include "vec3.h"
+#include "ray.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ofstream fout;
+    fout.open("output.ppm");
+    
+    int nx = 200;
+    int ny = 100;
+    
+    fout << "P3\n" << nx << " " << ny <<"\n255\n";
+    for(int j = ny-1; j >= 0; j--){
+        for(int i = 0; i < nx; i++){
+            
+            vec3 col(float(i)/float(nx),float(j)/float(ny),0.2);
+            
+            int ir = int(256*col[0]);
+            int ig = int(256*col[1]);
+            int ib = int(256*col[2]);
+            
+            fout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    
     return 0;
 }
